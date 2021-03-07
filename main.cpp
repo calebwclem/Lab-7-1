8b2703f987c883a909626a715de7399aeaa0ee13
calebwclem <79200015+calebwclem@users.noreply.github.com>
2021-03-06 18:31:04 -0800

    Created main
    Created main.cpp

---
+++ main.cpp
@@ -0,0 +1,30 @@
+//*****************
+//Program Name: Lab 7-1 (Simple Cryptography)
+//Author: Caleb Clements
+//IDE Used: Visual Studio
+//Program description: Writing a simple program that encrypts text to a textfile 
+// named through user input.
+//*****************
+#include <iostream>
+#include <map>
+
+
+using namespace std;
+
+void plagiarismReq();
+
+int main()
+{
+
+
+	plagiarismReq();
+	return 0;
+}
+
+//plagiarismReq(): Displays required text for plagiarism requirement in this course.
+//Arguments: None | Returns: None
+void plagiarismReq()
+{
+	cout << "This is my original work; apart from standard tutoring or class collaboration, "
+		<< "I neither received help nor copied this code from another source.";
+}
